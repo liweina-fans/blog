72d4461c69b211cc33bbedddb8a918db4fb2862e
liweina-fans <liweina.fans@gmail.com>
2019-09-08 17:01:32 +0800

    Add licence in blog dre

---
+++ docs/.vuepress/router/blog.js
@@ -0,0 +1,5 @@
+module.exports = {
+  title: "博客文章",
+  collapsable: false,
+  children: [['/blog/license','开源许可证之间的区别']]
+};
--- docs/.vuepress/router/index.js
+++ docs/.vuepress/router/index.js
@@ -1,9 +1,11 @@
 const echarts =  require('./echarts');
 const d3 = require('./d3');
 const vue = require('./vue'); 
+const blog = require('./blog')
 
 module.exports = [
     echarts,
     d3,
-    vue
+    vue,
+    blog
 ]
